28fd6d689b8f054535d72e0a7159ef240fbe67fa
BeolineNiyu <beolineniyukuri1@gmail.com>
2026-06-10 14:47:49 +0200

    CLUD champs

--- backend/afroagri/urls.py
+++ backend/afroagri/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from users.views import DashboardMinisterView, DashboardAdminView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/api/users/users/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/type-users/', include('type_user.urls')),
     path('api/users/', include('users.urls')),
+    path('api/DashboardMinister', DashboardMinisterView.as_view()),
+    path('api/DashboardMinister/', DashboardMinisterView.as_view()),
+    path('api/DashboardAdmin', DashboardAdminView.as_view()),
+    path('api/DashboardAdmin/', DashboardAdminView.as_view()),
     path('api/champs/', include('champs.urls')),
     path('api/plantes/', include('plantes.urls')),
     path('api/journal/', include('journal.urls')),
